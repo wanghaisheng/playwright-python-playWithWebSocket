14a8409173f9862500317d2a18c4a70c7217b46c
YusukeIwaki <iwaki@i3-systems.com>
2021-07-07 15:06:25 +0900

    Playwright connection via WebSocket

--- with_playwright_browser.py
+++ with_playwright_browser.py
@@ -1,11 +1,12 @@
 import contextlib
 import typing
-from playwright.sync_api import sync_playwright, Browser, BrowserType
+from playwright_remote_context_manager import sync_playwright_remote
+from playwright.sync_api import Browser, BrowserType
 
 
 @contextlib.contextmanager
 def playwright_browser(browser_type, **launch_params) -> typing.ContextManager[Browser]:
-    with sync_playwright() as playwright:
+    with sync_playwright_remote('ws://127.0.0.1:8080/ws') as playwright:
         _browser_type: BrowserType = getattr(playwright, browser_type)
         browser = _browser_type.launch(**launch_params)
         try:
