137bf13f7b849a6f00266685fcfabe37412737f1
YusukeIwaki <iwaki@i3-systems.com>
2021-07-07 22:02:34 +0900

    connect to heroku

--- ex.py
+++ ex.py
@@ -1,12 +1,12 @@
 from with_playwright_browser import playwright_browser
 from playwright.sync_api import Page
 
-with playwright_browser('chromium', headless=False) as browser:
+with playwright_browser('chromium') as browser:
     page: Page = browser.new_page()
     page.goto('https://github.com/YusukeIwaki')
     page.screenshot(path='YusukeIwaki.png')
 
-with playwright_browser('firefox', headless=False) as browser:
+with playwright_browser('firefox') as browser:
     page: Page = browser.new_page()
     page.goto('https://github.com/microsoft')
     page.screenshot(path='microsoft.png')
--- with_playwright_browser.py
+++ with_playwright_browser.py
@@ -2,11 +2,13 @@ import contextlib
 import typing
 from playwright_remote_context_manager import sync_playwright_remote
 from playwright.sync_api import Browser, BrowserType
+import os
 
 
 @contextlib.contextmanager
 def playwright_browser(browser_type, **launch_params) -> typing.ContextManager[Browser]:
-    with sync_playwright_remote('ws://127.0.0.1:8080/ws') as playwright:
+    ws_endpoint = os.getenv('PLAYWRIGHT_WS_URL', 'ws://127.0.0.1:8080/ws')
+    with sync_playwright_remote(ws_endpoint) as playwright:
         _browser_type: BrowserType = getattr(playwright, browser_type)
         browser = _browser_type.launch(**launch_params)
         try:
