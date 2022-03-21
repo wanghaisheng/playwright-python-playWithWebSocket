54633fad1997887fa166c3672513ae6d1e5fae12
wanghaisheng <edwin.uestc@gmail.com>
2022-03-21 17:31:36 +0800

    add requirement.txt

--- ex.py
+++ ex.py
@@ -5,8 +5,9 @@ with playwright_browser('chromium') as browser:
     page: Page = browser.new_page()
     page.goto('https://github.com/YusukeIwaki')
     page.screenshot(path='YusukeIwaki.png')
-
+# PLAYWRIGHT_WS_URL=wss://playwrightserver.herokuapp.com/ws python3 ex.py
 with playwright_browser('firefox') as browser:
     page: Page = browser.new_page()
     page.goto('https://github.com/microsoft')
     page.screenshot(path='microsoft.png')
+
